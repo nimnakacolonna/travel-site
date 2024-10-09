ecd15203662eb9ef3dee36aabef66ff5007284b4
Thisal Colonna <thisal@echonlabs.com>
2024-10-09 18:10:42 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const menus = document.querySelector("nav ul");
+const header = document.querySelector("header");
+const menuBtn = document.querySelector(".menu-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click", () => {
+    menus.classList.add("display");
+});
+
+closeBtn.addEventListener("click", () => {
+    menus.classList.remove("display");
+});
