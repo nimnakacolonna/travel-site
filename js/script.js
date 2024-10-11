85b308878116376d38fefd426e10141a280e613c
Thisal Colonna <thisal@echonlabs.com>
2024-10-11 19:33:47 +0530

    3 pages

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-const menus = document.querySelector("navbar");
-const header = document.querySelector("header");
-const menuBtn = document.querySelector(".menu-icon");
-const closeBtn = document.querySelector(".close-icon");
+const menuIcon = document.getElementById("menu-icon"); // Selects the menu icon (bx-menu)
+const closeIcon = document.getElementById("close-icon"); // Selects the close icon (bx-x)
+const header = document.querySelector(".header"); // Selects the navbar containing the links
+const checkbox = document.getElementById("check"); // Selects the checkbox
 
-menuBtn.addEventListener("click", () => {
-    menus.classList.add("display");
-});
+// menuBtn.addEventListener("click", () => {
+//     menus.classList.add("display");
+// });
 
-closeBtn.addEventListener("click", () => {
-    menus.classList.remove("display");
-});
+// closeBtn.addEventListener("click", () => {
+//     menus.classList.remove("display");
+// });
+
+//scroll sticky navbar 
+
+window.addEventListener("scroll",() =>{
+    if(document.documentElement.scrollTop > 20){
+        header.classList.add('sticky');
+    }else{
+        header.classList.remove("sticky");
+    }
+
+    
+})
