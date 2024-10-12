69693408d23d6e72b90910c6ee23ebd9997e7f0d
Thisal Colonna <thisal@echonlabs.com>
2024-10-13 03:57:30 +0530

    Testimonials Start

--- js/swiper.js
+++ js/swiper.js
@@ -38,5 +38,51 @@ const swiper2 = new Swiper(".swiper2", {
     speed: 400,
     
 
-    breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 2,
+        },
+        // when window width is >= 640px
+        968: {
+          slidesPerView: 3,
+        }
+      }
     });
+
+    
+const swiper3 = new Swiper(".swiper3", {
+  slidesPerView: 2,
+  spaceBetween:35,
+  slidesPerGroup:1,
+  loop: true,
+  fade:true,
+  centerSlide: true,
+  grabCursor: true,
+  loopfillGroupwithBlank:true,
+
+  autoplay:{
+      delay: 5000,
+  },
+  speed: 400,
+  
+
+  breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 480px
+      768: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 640px
+      968: {
+        slidesPerView: 2,
+      }
+    }
+  });
